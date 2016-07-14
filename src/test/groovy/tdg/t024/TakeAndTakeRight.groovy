0cb74eb314b9df0087991e438ee8aabb54ba75d0
The Daily Groovy <@DailyGroovyLang>
2016-07-14 18:44:32 +0300

    fix package layout

--- src/test/groovy/tdg/t024/TakeAndTakeRight.groovy
+++ src/test/groovy/tdg/t024/TakeAndTakeRight.groovy
@@ -1,9 +1,9 @@
-package tdg.t023.t024
+package tdg.t024
 
 import spock.lang.Specification
 
 
-class TakeWhile extends Specification {
+class TakeAndTakeRight extends Specification {
     def "test"() {
         given: "Need some elements from head or tail of the list? Try 'take*' methods #GroovyTip #groovylang"
 
