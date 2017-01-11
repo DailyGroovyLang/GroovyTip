277ba146e9e5ed7aa8876b2ab1ad4adc5be15729
The Daily Groovy <@DailyGroovyLang>
2017-01-11 21:11:34 +0300

    t030 StringTokenize

---
+++ src/test/groovy/tdg/t030/StringTokenize.groovy
@@ -0,0 +1,23 @@
+package tdg.t030
+
+import spock.lang.Specification
+
+
+/**
+ * When you need to split string by some characters try Groovy tokenize instead of String split
+ */
+class StringTokenize extends Specification {
+    def "test"() {
+        given:
+        def greeting = 'Hello, Groovy People'
+
+        when:
+        def tokens = greeting.tokenize(' ,')
+        //                              ^^ set of delimiters
+
+        then:
+        tokens == ['Hello', 'Groovy', 'People']
+        tokens == greeting.split(/(,| )/).findAll()
+        //                  regexp ^^      ^^ filter of empty tokens
+    }
+}
